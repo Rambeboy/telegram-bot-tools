81088332ea69b1fc7404dbc99d5beff9283a4f15
Nofan Rambe <kingindoslayer@gmail.com>
2025-03-05 05:19:42 +0700

    Update helper.js

--- app/src/utils/helper.js
+++ app/src/utils/helper.js
@@ -2,7 +2,6 @@ import readline from "readline";
 import promptSync from "prompt-sync";
 
 const prompt = promptSync();
-
 const clearLine = () => {
   readline.cursorTo(process.stdout, 0);
   readline.clearLine(process.stdout, 0);
@@ -10,13 +9,12 @@ const clearLine = () => {
 
 export const showLoginMenu = () => {
   console.log("\n=== LOGIN OPTIONS ===\n");
-  console.log("1. Login with API ID & API HASH");
+  console.log("1. Login with API ID & API Hash");
   console.log("2. Login with QR Code");
   console.log("3. Exit");
 
   process.stdout.write("\nChoose login method (1/2/3) : ");
-  clearLine();  
-  const choice = prompt("");
+  const choice = prompt(""); 
 
   if (choice === "3") {
     console.log("Exiting...");
