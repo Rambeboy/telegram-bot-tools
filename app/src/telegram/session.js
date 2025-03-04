29b21db52e95393993c1ab028e4e396f449b61ee
Nofan Rambe <kingindoslayer@gmail.com>
2025-03-05 04:31:51 +0700

    Update session.js

--- app/src/telegram/session.js
+++ app/src/telegram/session.js
@@ -1,34 +1,34 @@
-import fs from "fs-extra";
+import { readFileSync, writeFileSync, existsSync, unlinkSync } from "fs";
 
 const SESSION_FILE = "session.json";
 
 export const saveSession = (session) => {
   try {
-    fs.writeFileSync(SESSION_FILE, session);
+    writeFileSync(SESSION_FILE, session);
     console.log("Session has been saved successfully.");
   } catch (error) {
-    console.error("Failed to save session :", error);
+    console.error("Failed to save session:", error);
   }
 };
 
 export const loadSession = () => {
   try {
-    if (fs.existsSync(SESSION_FILE)) {
-      return fs.readFileSync(SESSION_FILE, "utf-8");
+    if (existsSync(SESSION_FILE)) {
+      return readFileSync(SESSION_FILE, "utf-8");
     }
   } catch (error) {
-    console.error("Failed to load session :", error);
+    console.error("Failed to load session:", error);
   }
   return "";
 };
 
 export const deleteSession = () => {
   try {
-    if (fs.existsSync(SESSION_FILE)) {
-      fs.unlinkSync(SESSION_FILE);
+    if (existsSync(SESSION_FILE)) {
+      unlinkSync(SESSION_FILE);
       console.log("Telegram session has been deleted.");
     }
   } catch (error) {
-    console.error("Failed to delete session :", error);
+    console.error("Failed to delete session:", error);
   }
 };
