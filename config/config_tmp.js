738337952e91960c2ab8ece3cd92724940ef53aa
Nofan Rambe <kingindoslayer@gmail.com>
2025-03-04 16:14:31 +0700

    Update config_tmp.js

--- config/config_tmp.js
+++ config/config_tmp.js
@@ -1,4 +1,4 @@
 export class Config {
-  static TELEGRAM_APP_ID = undefined; // Ganti dengan APP ID Anda
-  static TELEGRAM_APP_HASH = "undefined"; // Ganti dengan APP HASH Anda
+  static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
+  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
 }
